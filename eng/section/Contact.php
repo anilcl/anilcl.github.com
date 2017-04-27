11bb9b4ea4414303dd1302370574378b1f762a7f
anilcl <demirpence@hacettepe.edu.tr>
2017-04-27 20:12:31 -0300

    Add files via upload

---
+++ eng/section/Contact.php
@@ -0,0 +1,103 @@
+<section class="section section--Contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row">
+
+
+                    <div class="col-sm-7">
+                        <h3 class="section-title">CONTACT</h3>
+
+                        <p class="mark">We love what we do.</p>
+
+                        <p>Let’s work together, so your projects become the best. To have more information, or even to
+                            say Hello, contact us.</p>
+
+                        <div class="address" itemscope itemtype="http://schema.org/LocalBusiness">
+                            <div class="address_action">
+                                <a itemprop="email" href="mailto:hello@interacthings.com">hello@interacthings.com</a>
+                                <a itemprop="telephone" href="tel:+902129510091">+90 212 951 0091</a>
+                            </div>
+
+                            <address itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                                <span class="line"
+                                      itemprop="streetAddress">Arap Cami Mah, Yanıkkapı Sok, No:3, Suma Han</span><span
+                                    class="hidden-sm hidden-md hidden-lg">,</span>
+                                <span itemprop="postalCode">34420</span>,
+                                <span itemprop="addressLocality">Beyoğlu</span>,
+                                <span itemprop="addressRegion">Istanbul</span>,
+                                <span itemprop="addressCountry"><abbr title="Turkey">TR</abbr></span>
+                            </address>
+                        </div>
+
+                    </div>
+
+
+                    <div class="social col-sm-4 col-sm-offset-1">
+                        <h3 class="section-title">FOLLOW US</h3>
+
+                        <ul class="social-list">
+                            <li>
+                                <a class="social-list_link social-list_link--facebook" target="_blank"
+                                   href="https://www.facebook.com/interacthingss">
+                                    <svg class="icon icon-facebook-square" viewBox="0 0 20 20">
+                                        <use xlink:href="../img/icon.svg#icon-facebook-square"></use>
+                                    </svg>
+                                    facebook
+                                </a>
+                            </li>
+                            <li>
+                                <a class="social-list_link social-list_link--twitter" target="_blank"
+                                   href="https://twitter.com/interacthings">
+                                    <svg class="icon icon-twitter" viewBox="0 0 19 20">
+                                        <use xlink:href="../img/icon.svg#icon-twitter"></use>
+                                    </svg>
+                                    twitter
+                                </a>
+                            </li>
+                            <li>
+                                <a class="social-list_link social-list_link--dribbble" target="_blank"
+                                   href="https://dribbble.com/interacthings">
+                                    <svg class="icon icon-dribbble" viewBox="0 0 20 20">
+                                        <use xlink:href="../img/icon.svg#icon-dribbble"></use>
+                                    </svg>
+                                    dribbble
+                                </a>
+                            </li>
+                            <li>
+                                <a class="social-list_link social-list_link--github" target="_blank"
+                                   href="https://github.com/interacthings">
+                                    <svg class="icon icon-github" viewBox="0 0 20 20">
+                                        <use xlink:href="../img/icon.svg#icon-github"></use>
+                                    </svg>
+                                    github
+                                </a>
+                            </li>
+                            <li>
+                                <a class="social-list_link social-list_link--instagram" target="_blank"
+                                   href="http://instagram.com/interacthings">
+                                    <svg class="icon icon-instagram" viewBox="0 0 20 20">
+                                        <use xlink:href="../img/icon.svg#icon-instagram"></use>
+                                    </svg>
+                                    instagram
+                                </a>
+                            </li>
+                            <li>
+                                <a class="social-list_link social-list_link--github" target="_blank"
+                                   href="http://jo.interacthings.com/">
+                                    <svg class="icon icon-plus" viewBox="0 0 16 20">
+                                        <use xlink:href="../img/icon.svg#icon-plus"></use>
+                                    </svg>
+                                    Join
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /section--We -->
---
+++ eng/section/Intro.php
@@ -0,0 +1,32 @@
+<section class="section section--Intro">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+
+
+                <header class="site-header">
+                    <div class="site-logo">
+                        <svg>
+                            <use xlink:href="../img/logo.svg#Logo"></use>
+                        </svg>
+                    </div>
+
+                    <a class="site-lang" href="http://interacthings.com/">TR</a>
+                </header>
+
+
+                <div class="slogan">
+                    <div class="row">
+                        <div class="col-xs-10">
+                            <h1 class="slogan_title">We design & build aesthetically impressive, easy-to-use
+                                interfaces.</h1>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /section--Intro -->
